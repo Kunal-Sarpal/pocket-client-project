27c71394898aa611cf1225064cbd0fc0ac4c343b
user.name : "Kunal-Sarpal <user.email: "sarpalkunal7@gmail.com>
2024-12-28 15:28:26 +0530

    ui update - 4

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -14,15 +14,18 @@ const Admin = () => {
                 </section>
 
                 {/* Order Management Section */}
-                <section className="w-full lg:flex-1 bg-white border border-gray-300 shadow-lg rounded-md p-4 lg:p-6">
+                <div className=' overflow-x-auto'>
+
+                <section className="w-full lg:flex-1 bg-white border border-gray-300 shadow-lg rounded-md p-4 lg:p-6 min-w-[900px] overflow-x-auto">
                     <ManageOrders />
                 </section>
+                </div>
             </div>
 
             {/* Bottom Section */}
             <div className="w-full flex flex-wrap lg:flex-nowrap overflow-x-auto border-t border-gray-300 p-4 lg:p-8 gap-4 lg:gap-6">
                 {/* Update Products Section */}
-                <section className="w-full lg:flex-1 bg-white border border-gray-300 shadow-lg rounded-md p-4 lg:p-6 min-w-[600px]">
+                <section className="w-full lg:flex-1 bg-white border border-gray-300 shadow-lg rounded-md p-4 lg:p-6 min-w-[900px]">
                     <UpdateProducts />
                 </section>
             </div>
